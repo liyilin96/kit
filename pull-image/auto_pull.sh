921fe44750f6a4afc75cbc53884608d2a4831b14
liyilin96 <2062523608@qq.com>
2022-09-21 14:39:41 +0800

    Create auto_pull.sh

---
+++ pull-image/auto_pull.sh
@@ -0,0 +1,9 @@
+src=$1
+dest=$2
+
+echo "source image is $src"
+echo "descination image is $dest"
+# containerd for cri
+ctr -n k8s.io i pull --all-platforms $src 
+ctr -n k8s.io i tag $src $dest
+ctr -n k8s.io i push  $dest --plain-http=true -u admin:admin
