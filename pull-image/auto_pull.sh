bc166cf4fa5a9403818f0ff0230c0cab91ab3f72
liyilin96 <2062523608@qq.com>
2022-10-14 09:53:29 +0800

    Update auto_pull.sh
    change password

--- pull-image/auto_pull.sh
+++ pull-image/auto_pull.sh
@@ -1,9 +1,11 @@
 src=$1
 dest=$2
+user=USER
+pass=PASSWORD
 
 echo "source image is $src"
 echo "descination image is $dest"
 # containerd for cri
 ctr -n k8s.io i pull --all-platforms $src 
 ctr -n k8s.io i tag $src $dest
-ctr -n k8s.io i push  $dest --plain-http=true -u USER:PASSWORD
+ctr -n k8s.io i push  $dest --plain-http=true -u $user:$pass
