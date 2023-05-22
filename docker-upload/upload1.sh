01f40ed1260ba567efe8d5adea7de5d396f2c387
liyilin96 <2062523608@qq.com>
2023-05-22 18:08:35 +0800

    Update upload1.sh

--- docker-upload/upload1.sh
+++ docker-upload/upload1.sh
@@ -1,3 +1,6 @@
+# local hub
 for file in `ls`; do echo $file; docker load -i $file; done
 
-docker images | grep registry.yourharbor.com | awk '{ print $1 ":" $2}'
+# public hub
+docker images | grep registry.xsky.com | awk '{ print $1 ":" $2}' | awk '{ print $0; gsub(/registry.local/, "registry.public.com/project1"); print $1}'| awk '{if(NR%2!=0)ORS=" ";else ORS="\n"}1' | awk '{ print "docker tag " $1 " " $2 ";\n docker push " $2 ";\n"}' | bash
+
