858058bc6fb8ac9c82094f27230064df899d77a9
liyilin96 <2062523608@qq.com>
2023-05-22 18:09:16 +0800

    Update upload1.sh

--- docker-upload/upload1.sh
+++ docker-upload/upload1.sh
@@ -2,5 +2,5 @@
 for file in `ls`; do echo $file; docker load -i $file; done
 
 # public hub
-docker images | grep registry.xsky.com | awk '{ print $1 ":" $2}' | awk '{ print $0; gsub(/registry.local/, "registry.public.com/project1"); print $1}'| awk '{if(NR%2!=0)ORS=" ";else ORS="\n"}1' | awk '{ print "docker tag " $1 " " $2 ";\n docker push " $2 ";\n"}' | bash
+docker images | grep registry.local | awk '{ print $1 ":" $2}' | awk '{ print $0; gsub(/registry.local/, "registry.public.com/project1"); print $1}'| awk '{if(NR%2!=0)ORS=" ";else ORS="\n"}1' | awk '{ print "docker tag " $1 " " $2 ";\n docker push " $2 ";\n"}' | bash
 
